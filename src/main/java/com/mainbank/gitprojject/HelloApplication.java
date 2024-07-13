4655b488300e60966706c724e2f6063760fa7afd
saif juini <saif.juini2@gmail.com>
2024-07-13 11:50:01 +0100

    changes stashed applying them

--- src/main/java/com/mainbank/gitprojject/HelloApplication.java
+++ src/main/java/com/mainbank/gitprojject/HelloApplication.java
@@ -12,7 +12,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+<<<<<<< Updated upstream
         stage.setTitle("Hello saif git project!");
+=======
+        stage.setTitle("news main changes to try git stash Hello saif git project!");
+>>>>>>> Stashed changes
         stage.setScene(scene);
         stage.show();
     }
