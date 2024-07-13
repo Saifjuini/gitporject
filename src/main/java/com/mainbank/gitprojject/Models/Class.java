dd98017cb481696227059422b26d8590c5b1df54
saif juini <saif.juini2@gmail.com>
2024-07-13 10:25:45 +0100

    new class class

---
+++ src/main/java/com/mainbank/gitprojject/Models/Class.java
@@ -0,0 +1,13 @@
+package com.mainbank.gitprojject.Models;
+
+public class Class {
+    String name;
+    String description;
+    int age;
+    String gender;
+    String address;
+    String phone;
+    public Class(String name, String description, int age, String gender, String address, String phone) {}
+
+    public Class() {}
+}
